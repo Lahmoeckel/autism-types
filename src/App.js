ee661ef5cd49f7e21556d49eb48b74df63a2bf1f
Lahmoeckel <lais.menassa@gmail.com>
2024-04-19 12:59:58 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+function Link({ text }) {
+  // Função para associar ação ao clicar no botão 1
+  const handleClickBotao1 = () => {
+    // Redireciona para o link correspondente ao botão 1
+    window.location.href = "link_do_botao_1";
+  };
+
+  // Função para associar ação ao clicar no botão 2
+  const handleClickBotao2 = () => {
+    // Redireciona para o link correspondente ao botão 2
+    window.location.href = "link_do_botao_2";
+  };
+
+  // Função para associar ação ao clicar no botão 3
+  const handleClickBotao3 = () => {
+    // Redireciona para o link correspondente ao botão 3
+    window.location.href = "link_do_botao_3";
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mainDiv'> 
+      {/* <p>
+        <code>{text}</code>
+      </p> */}
+      
+      <div className="buttonsDiv">
+        <button className="btn1" onClick={handleClickBotao1}>Botão 1</button>
+        <button className="btn2" onClick={handleClickBotao2}>Botão 2</button>
+        <button className="btn3" onClick={handleClickBotao3}>Botão 3</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Link;
---
+++ src/Link.js
@@ -0,0 +1,15 @@
+
+function Link({text}) {
+    return (
+        <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            {text}
+        </a>
+    );
+}
+
+export default Link;
