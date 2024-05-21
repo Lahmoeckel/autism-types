760d6b167c42318b049c17bc17ab6e52e8debbb5
Lahmoeckel <lais.menassa@gmail.com>
2024-05-21 15:58:53 +0100

    new images

---
+++ src/Home.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import "./App.css";
+import Born from "./assets/Born.jpg";
+//import "../src/Button2Page.js"
+
+function Home() {
+  const navigate = useNavigate();
+
+  useEffect(() => { //Beggining of react js lifecycle
+    //alert("Test");
+  }, []);
+
+  return (
+    <div className="mainDiv">
+      <h5>
+        Autism Types
+      </h5>
+
+      
+
+      <div className="buttonsDiv">
+        {/* Botão 1 com texto Type 1 */}
+       <ul><button className="btn btn1" onClick={() => { navigate("/button1") }}>Type 1</button></ul>
+        {/* Botão 2 com texto Type 2 */}
+        <button className="btn btn2" onClick={() => { navigate("/button2") }}>Type 2</button>
+        {/* Botão 3 com texto Type 3 */}
+        <button className="btn btn3" onClick={() => { navigate("/button3") }}>Type 3</button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
